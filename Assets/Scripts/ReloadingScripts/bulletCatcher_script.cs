224fc369884d015adb691c3511336ecfa09b678c
Lyonloi <lyonloi2002@gmail.com>
2015-03-31 14:46:13 -0500

    3/31/15

--- Assets/Scripts/ReloadingScripts/bulletCatcher_script.cs
+++ Assets/Scripts/ReloadingScripts/bulletCatcher_script.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class bulletCatcher_script : MonoBehaviour {
 
+	//access to the cannon script
 	public Cannons_script Cs;
+
+	//reference the explosion prefeb
+	public GameObject explosion;
 
 	// Use this for initialization
 	void Start () {
@@ -16,14 +20,22 @@ public class bulletCatcher_script : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider C){
+		//if the ground catch a rock
 		if (C.tag == "bullet") {
+			//tell the cannon that it can reload a rock
 			Cs.RockOut ();
+			//make the rock doesn't kill the enemies when it stays on the ground
 			C.gameObject.tag = "rock";
 
 				} 
+		//if the ground catch a rock
 		else if (C.tag == "bomb") {
+			//tell the cannon that it can reload a bomb
 			Cs.BombOut ();
+			//make an explosion
+			Instantiate(explosion, C.gameObject.transform.position, C.gameObject.transform.rotation);
 			Destroy(C.gameObject);
+
 				} 
 		else if (C.tag == "oil") {
 			Cs.OilOut();
