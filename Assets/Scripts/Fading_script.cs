832b0218e38d327179e928c255dfaeb09caf4a0e
Lyonloi <lyonloi2002@gmail.com>
2015-04-22 14:28:26 -0500

    22

---
+++ Assets/Scripts/Fading_script.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fading_script : MonoBehaviour {
+
+	public Texture2D fadeOutTexture;
+	public float fadeSpeed = 0.8f;
+
+	int drawDepth = -1000; 	//the texture's orderin the draw hierarchy: a low number means it renders on top
+	float alpha = 1.0f; 	//the texture's alpha value between 0 and 1
+	int fadeDir = -1;		//the direction to fade: in = -1 out = 1
+
+	void OnGUI(){
+
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;
+
+		alpha = Mathf.Clamp01(alpha);
+
+		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+		GUI.depth = drawDepth;
+		GUI.DrawTexture(new Rect (0,0, Screen.width, Screen.height), fadeOutTexture);
+
+
+	}
+
+	public float BeginFade(int direction){
+
+		fadeDir = direction;
+		return(fadeSpeed);
+
+	}
+
+	void OnLevelWasLoaded(){
+		BeginFade(-1);
+	}
+	
+}
---
+++ Assets/Scripts/MainMenu_Script.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NextLevel(){
+
+			//fade out the game and load a new level
+			float fadeTime = GameObject.Find ("_GM").GetComponent<Fading_script> ().BeginFade (1);
+			
+			Application.LoadLevel (1);
+	}
+
+
+
+
+
+}
