148dd9f128796ec66671cf58a08aea07a168ee20
Lyonloi <lyonloi2002@gmail.com>
2015-05-12 20:05:26 -0500

    12

--- Assets/Scripts/EnemyScripts/EnemySpawning_script.cs
+++ Assets/Scripts/EnemyScripts/EnemySpawning_script.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 
 public class EnemySpawning_script : MonoBehaviour {
+
+	//Vars for refencing this to GameManager
+	GameManager_script GMs;
+
 	//vars of spawning timer
 	float SpawningCount = 0f;
 	public float SpawningTime = 3f;
@@ -15,7 +19,7 @@ public class EnemySpawning_script : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		GMs = GameObject.FindGameObjectWithTag ("GM").GetComponent<GameManager_script>();
 	}
 	
 	// Update is called once per frame
@@ -33,12 +37,36 @@ public class EnemySpawning_script : MonoBehaviour {
 
 	//spawn an emeny at a ramdom spawnpoint
 	void SpawnAnEnemy (){
+
+		if (GMs.currentLV == 1) {
+			//spwan an enemy after 0 second
+			StartCoroutine( Spawn(0.1f));
+		}
+
+		if (GMs.currentLV == 2) {
+			//spwan an enemy after 0 second
+			StartCoroutine( Spawn(0.1f));
+		}
+
+		if (GMs.currentLV == 3) {
+
+			//spwan an enemy after 0 second
+			StartCoroutine( Spawn(0.1f));
+			//spwan an enemy after 1 second
+			StartCoroutine( Spawn(1f));
+		}
+		
+	}
+
+	IEnumerator Spawn(float S){
+	
+		yield return new WaitForSeconds(S);
+
 		//get a random # based on the length of the array
 		pointSelect = Random.Range (0, spawnpoints.Length);
-		//Instantiate a prefad based on the number just got
-		Instantiate(SpawnTarget, spawnpoints[pointSelect].position, spawnpoints[pointSelect].rotation);
-
 		
+		//Instantiate a prefad based on the number just got
+		Instantiate (SpawnTarget, spawnpoints [pointSelect].position, spawnpoints [pointSelect].rotation);
 	}
 
 	void OnTriggerStay(Collider C){
--- Assets/Scripts/GameManager_script.cs
+++ Assets/Scripts/GameManager_script.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class GameManager_script : MonoBehaviour {
+
 	//vars for Gate condition
 	int gateHP = 100;
 	public Text gateHP_T;
@@ -10,6 +11,13 @@ public class GameManager_script : MonoBehaviour {
 	//Vars for Scores;
 	int score = 0;
 	public Text score_T;
+
+
+
+	//vars for levels
+	public Text lv_T;
+	public int currentLV = 1;
+	int levelCount = 1;
 
 	//Vars for lose screen
 	public GameObject loseScreen;
@@ -24,9 +32,10 @@ public class GameManager_script : MonoBehaviour {
 	void Update () {
 		//keep the HP and Score Updated
 		gateHP_T.text = "Gate Condition: " + gateHP + "%";
-		score_T.text = "Coins: " + score;
+		score_T.text = "Score: " + score;
+		lv_T.text = "LV: " + currentLV;
 
-		if (score >= 2500) {
+		if (score >= 2000) {
 			winText.text = "You won! Thanks for playing;";
 			loseScreen.gameObject.SetActive(true);
 			Time.timeScale = 0.0f;
@@ -36,7 +45,15 @@ public class GameManager_script : MonoBehaviour {
 			loseScreen.gameObject.SetActive(true);
 			Time.timeScale = 0.0f;
 		}
-		
+
+		if (levelCount >= 400) {
+			LevelUp();	
+		}
+	}
+
+	void LevelUp(){
+		currentLV += 1;
+		levelCount -= 400;
 	}
 
 	public void HPDown (int a){
@@ -47,6 +64,7 @@ public class GameManager_script : MonoBehaviour {
 	public void AddScore(int S){
 		//increases the score
 		score += S;
+		levelCount += S;
 	}
 
 	public void Restart(){
